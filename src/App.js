fd34850cf5404a5cd3d4ba74062776006685b05e
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-08-12 18:43:13 +0700

    feat: Route to each pets

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ import {
 const appRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
-      <Route path="/" element={<HomePage />}>
-        <Route path=":type" element={<HomePage />} />
-      </Route>
-      <Route path="/search" element={<SearchPage />} />
+      <Route index element={<HomePage />} />
+      <Route path=":type" element={<HomePage />} />
       <Route path="/:type/:id" element={<PetDetailsPage />} />
+
+      <Route path="/search" element={<SearchPage />} />
       <Route path="/:type/:id/not-found" element={<PetDetailsNotFound />} />
     </Route>
   )
